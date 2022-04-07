361ecfd551313b8e4e6e2cc9837be0d46f0c9cda
Hallisson Santos <hallissonerick1595@gmail.com>
2022-04-06 21:04:27 -0300

    commit inicial

---
+++ galeria.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>CineMarvel</title>
+</head>
+
+<body>
+    <nav class="nav-extended red lighten-1">
+        <div class="nav-wrapper">
+            <!-- <a href="#" class="brand-logo center">CineMarvel</a> -->
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CineMarvel</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent red darken-2">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/gDKjDC7udT18B1Do4ePlsDk2Rug.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> Homem de Ferro</strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 9,5</p>
+                    <p>Tony Stark é um industrial bilionário, que também é um brilhante inventor. Ao ser sequestrado ele é obrigado por
+                         terroristas a construir uma arma devastadora mas, ao invés disto, constrói uma armadura de alta tecnologia que permite 
+                         que fuja de seu cativeiro. A partir de então ele passa a usá-la para combater o crime, sob o alter ego do Homem de Ferro.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/do5wx1X3qJ6JIil8wEIYJiuWUhj.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> O Incrivel Hulk</strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 8,6</p>
+                    <p>Vivendo escondido e longe de Betty Ross, a mulher que ama, o cientista Bruce Banner 
+                        busca um meio de retirar a radiação gama que está em seu sangue. Ao mesmo tempo ele precisa 
+                        fugir da perseguição do general Ross, seu grande inimigo, e da máquina militar que 
+                        tenta capturá-lo, na intenção de explorar o poder que faz com que Banner se transforme 
+                        no Hulk.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/vzROjQbgKWMVf2EldXipCcjpuBL.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> Homem de Ferro 2</strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 8,5</p>
+                    <p>Em um mundo ciente da existência do Homem de Ferro, o inventor
+                         bilionário Tony Stark sofre pressão de todos os lados para 
+                         compartilhar sua tecnologia com as forças armadas. Ele resiste
+                          para divulgar os segredos de sua inigualável armadura, com medo
+                           de que estas informações caiam nas mãos erradas.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/yI6jOYR9xDpxXPKAO17YuIBfROd.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> Thor </strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 8,5</p>
+                    <p>Como filho de Odin, rei dos deuses nórdicos, Thor logo herdará o trono de Asgard de seu idoso pai. 
+                        Porém, no dia de sua coroação, Thor reage com brutalidade quando os inimigos dos deuses entram no
+                         palácio violando o tratado. Como punição, Odin manda Thor para a Terra. Enquanto seu irmão Loki conspira 
+                         em Asgard, Thor, agora sem seus poderes, enfrenta sua maior ameaça.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/f976JQnjMhCTa1yLXz7UH1pV4DG.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> Capitão América</strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 8,5</p>
+                    <p>Durante a Segunda Guerra Mundial, Steve Rogers é um homem doente, do Brooklyn, que se transforma
+                         no super soldado Capitão América para ajudar nos esforços de guerra. Rogers deve parar o Caveira 
+                         Vermelha, o implacável chefe de armas de Adolf Hitler e líder de uma organização que pretende usar
+                          um misterioso mecanismo de incalculáveis poderes para dominar o mundo.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/original/j9hwS307Zlc5mQTbAnwV75vXG0H.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite</i></a>
+                </div>
+                <div class="card-content">
+                    <p><strong> Os Vingadores</strong> </p>
+                    <p class="valign-wrapper"> <i class="material-icons amber-text">star</i> Avaliação: 8,5</p>
+                    <p>Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da
+                         S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-herói
+                         s que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor,
+                          Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de 
+                          lado e agir como um grupo em prol da humanidade.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    < <div id="test1" class="col s12">Test 1</div>
+        <div id="test2" class="col s12">Test 2</div>
+        <div id="test3" class="col s12">Test 3</div>
+        <div id="test4" class="col s12">Test 4</div>
+</body>
+
+</html>
