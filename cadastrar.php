6d794ba700c1d5d61d5265e2299ecbc7c8007c55
Hallisson Santos <hallissonerick1595@gmail.com>
2022-04-07 21:04:42 -0300

    Ajustes no CSS

--- cadastrar.php
+++ cadastrar.php
@@ -1,38 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>CineMarvel</title>
-</head>
+<?php include "cabecalho.php" ?>
 
 <body>
     <nav class="nav-extended red lighten-1">
         <div class="nav-wrapper">
             <!-- <a href="#" class="brand-logo center">CineMarvel</a> -->
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="galeria.php">Galeria</a></li>
-                <li><a href="cadastrar.php">Cadastrar</a></li>
+            <ul id="nav-mobile" class="right ">
+                <li>
+                    <a href="galeria.php">Galeria</a>
+                </li>
+                <li class="active">
+                    <a href="cadastrar.php">Cadastrar</a>
+                </li>
             </ul>
         </div>
         <div class="nav-header center">
             <h1>CineMarvel</h1>
-        </div>
-        <div class="nav-content">
-            <ul class="tabs tabs-transparent red darken-2">
-                <li class="tab"><a class="active" href="#test1">Todos</a></li>
-                <li class="tab"><a href="#test2">Assistidos</a></li>
-                <li class="tab"><a href="#test3">Favoritos</a></li>
-            </ul>
         </div>
     </nav>
 
